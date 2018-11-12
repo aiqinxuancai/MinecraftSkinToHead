77198ade5f611a8bba58c871b8310b05ab6ef476
爱琴炫彩 <aiqinxuancai@163.com>
2018-11-12 13:48:31 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MinecraftSkinToHead
 
-c#
 
-我的世界的标准皮肤（单双层）转出一个16x16 32x32 64x64...这样的格式打大头贴
+
+Convert Minecraft's standard skin (single, double layer) to a 16x16 32x32 64x64... format avatar.
--- SkinToHead/Program.cs
+++ SkinToHead/Program.cs
@@ -22,20 +22,20 @@ namespace SkinToHead
         /// <summary>
         /// 从skin转换到头像
         /// </summary>
-        /// <param name="_image">skin文件的路径</param>
-        /// <param name="_size">size必须是8的倍数 16 32 64 ...</param>
-        static bool SkinToHeadFile(string _image, int _size)
+        /// <param name="fileName">skin文件的路径</param>
+        /// <param name="targetSize">size必须是8的倍数 16 32 64 ...</param>
+        static Bitmap SkinToHeadFile(string fileName, int targetSize)
         {
-            if (_size % 8 != 0) //不等于8的倍数
+            if (targetSize % 8 != 0) //不等于8的倍数
             {
-                return false;
+                throw new Exception("targetSize must be a multiple of 8.");
             }
 
-            int size = _size / 8;
+            int size = targetSize / 8;
 
-            Bitmap head = new Bitmap(_size, _size); //创建正方形画布
+            Bitmap head = new Bitmap(targetSize, targetSize); //创建正方形画布
             Graphics g = Graphics.FromImage(head);  //从画布创建图形
-            System.Drawing.Bitmap skin = new Bitmap(_image); //载入skin图片
+            System.Drawing.Bitmap skin = new Bitmap(fileName); //载入skin图片
 
             //取得头部的8*8位置，并按照比例填充矩形
             for (int x = 8; x <= 15; x++)
@@ -49,8 +49,8 @@ namespace SkinToHead
 
             //存储
             g.Save(); 
-            head.Save("a.png");
-            return true;
+            //head.Save("a.png");
+            return head;
 
 
         }
